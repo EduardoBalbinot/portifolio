245999762a1c85ac312f4e163a1856b67c4de983
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2025-10-16 00:47:51 -0300

    fix: email on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Jobs from "../data/jobs";
 
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center overflow-hidden">
+    <div className="flex flex-col items-center overflow-hidden" >
       <div className="flex flex-col xl:flex-row items-center gap-[7vw]">
         <div>
           <h1 className="xl:text-[2vw] text-center xl:text-left">Eduardo Balbinot</h1>
@@ -26,7 +29,7 @@ export default function Home() {
             height={300}
           />
         </div>
-        <div>
+        <div className="hidden xl:block">
           <h2 className="xl:text-[2vw] text-center xl:text-left">Contato</h2>
           <p className="xl:text-[1vw] text-center xl:text-left">eduardo.balbinot.dev@gmail.com</p>
         </div>
@@ -34,7 +37,7 @@ export default function Home() {
         <h3 className="mt-40 mb-8 font-medium text-3xl text-center xl:text-left">Alguns projetos em que trabalhei</h3>
       <div className="flex flex-wrap gap-4 w-full xl:w-[1090px] xl">
         {Jobs.map((job) => (
-          <a href={job.link} target="_blank" key={job.title} className="m-4 p-4 bg-fuchsia-50 rounded-lg w-[500px] hover:scale-105 transition-transform">
+          <a href={job.link} target="_blank" key={job.title} className="m-4 p-4 bg-fuchsia-50 rounded-lg w-[500px] hover:scale-105 transition-transform jobElement">
             <Image 
               src={job.image} 
               alt={job.title} 
